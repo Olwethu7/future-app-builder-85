cccbed40471fbdd1e8101ef0f31d97fbf0be891c
Amahle Siphumelele Hadebe <130764109+ST10396198@users.noreply.github.com>
2025-11-12 16:43:56 +0200

    Update Search.tsx

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -7,6 +7,7 @@ import { useQuery } from "@tanstack/react-query";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Badge } from "@/components/ui/badge";
 
+
 interface Room {
   id: string;
   name: string;
