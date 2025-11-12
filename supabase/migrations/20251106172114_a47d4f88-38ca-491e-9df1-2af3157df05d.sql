f7199c0007e4d56fb2d754bd71772796c2d9e294
Olwethu7 <132436323+Olwethu7@users.noreply.github.com>
2025-11-12 16:26:13 +0200

    Update 20251106172114_a47d4f88-38ca-491e-9df1-2af3157df05d.sql

--- supabase/migrations/20251106172114_a47d4f88-38ca-491e-9df1-2af3157df05d.sql
+++ supabase/migrations/20251106172114_a47d4f88-38ca-491e-9df1-2af3157df05d.sql
@@ -1,6 +1,7 @@
 -- One-time cleanup: Delete all user-related data
 -- This allows fresh start for new user registrations
 
+
 -- Delete in correct order due to potential dependencies
 DELETE FROM bookings;
 DELETE FROM saved_accommodations;
