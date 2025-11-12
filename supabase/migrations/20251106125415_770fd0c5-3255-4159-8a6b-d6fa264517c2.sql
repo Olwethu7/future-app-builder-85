6ff8d2bd913590dcb700998f7c894aa86ff4dc9c
Olwethu7 <132436323+Olwethu7@users.noreply.github.com>
2025-11-12 16:25:58 +0200

    Update 20251106125415_770fd0c5-3255-4159-8a6b-d6fa264517c2.sql

--- supabase/migrations/20251106125415_770fd0c5-3255-4159-8a6b-d6fa264517c2.sql
+++ supabase/migrations/20251106125415_770fd0c5-3255-4159-8a6b-d6fa264517c2.sql
@@ -1,3 +1,4 @@
 -- Remove the problematic trigger and function that require pg_net extension
+
 DROP TRIGGER IF EXISTS on_booking_created ON public.bookings;
 DROP FUNCTION IF EXISTS public.trigger_send_booking_notification();
